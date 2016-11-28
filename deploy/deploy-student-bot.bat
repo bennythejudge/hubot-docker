cf6a8f8b47fb4a0773ac3092b1835b9aaa13c83d
Sean P. Kane <skane@newrelic.com>
2016-11-28 09:02:33 -0800

    Add basic Docker compose support

--- deploy/deploy-student-bot.bat
+++ deploy/deploy-student-bot.bat
@@ -1,13 +1,13 @@
 # Windows Batch File
 
-#
 # Set the 3 variables below as instructed in class
+# or edit and run the deploy-env.bat file
 #
 # You must edit this file and set the
 # STUDENT_ID, SLACK_TOKEN, & HUB_USER variables correctly.
-set STUDENT_ID="000"
-set SLACK_TOKEN="000"
-set HUB_USER="000"
+#set STUDENT_ID="000"
+#set SLACK_TOKEN="000"
+#set HUB_USER="000"
 
 
 set BIND_ADDRESS="0.0.0.0"
--- deploy/deploy-student-bot.sh
+++ deploy/deploy-student-bot.sh
@@ -1,11 +1,5 @@
 #/bin/bash -xv
 
-
-# Set the 3 variables below as instructed
-export STUDENT_ID="000"
-export SLACK_TOKEN="000"
-export HUB_USER="000"
-
 if [ ${STUDENT_ID} == "000" ] || [ ${SLACK_TOKEN} == "000" ] || [ ${HUB_USER} == "000" ]; then
   echo
   echo "You must edit this file and set the"
